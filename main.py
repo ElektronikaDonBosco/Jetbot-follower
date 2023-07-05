6922fb24f646a7ffc2995eecb4f27d57dda53ab0
Mikel Aldalur <mikelalda13@gmail.com>
2023-07-05 22:52:28 +0200

    Fix frame not detected

--- main.py
+++ main.py
@@ -45,55 +45,55 @@ def main():
 
     while True:
         img = camera.Capture()
-        if img == None:
-            continue
-        height, width, channels = img.shape
-        detections = net.Detect(img)
-        # if render_img:
-        #     display.Render(img)
-        #     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
-        for detection in detections:
-            class_id = names[detection.ClassID]
-            x1 = detection.Left/width 
-            y1 = detection.Top/height
-            x2 = detection.Right/width
-            y2 = detection.Bottom/height
-            if class_id in gv.DETECTIONS:
-                color = transfer("")
-                # Si la persona detectada tiene algo verde
-                if color == "1":
-                    check = True
-                    lower = np.array([0, 0, 330], np.uint8)
-                    upper = np.array([180, 255, 30], np.uint8)
-                    break
-                # Si la persona detectada tiene algo rojo
-                elif color == "2":
-                    check = True
-                    lower = np.array([0, 0, 90], np.uint8)
-                    upper = np.array([180, 255, 150], np.uint8)
-                    break
-                # Si la persona detectada tiene algo azul
-                elif color == "3":
-                    check = True
-                    lower = np.array([0, 0, 210], np.uint8)
-                    upper = np.array([180, 255, 270], np.uint8)
-                    break
-                else:
-                    check = False
+        if img != None:
+            print('image is captured')
+            height, width, channels = img.shape
+            detections = net.Detect(img)
+            # if render_img:
+            #     display.Render(img)
+            #     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+            for detection in detections:
+                class_id = names[detection.ClassID]
+                x1 = detection.Left/width 
+                y1 = detection.Top/height
+                x2 = detection.Right/width
+                y2 = detection.Bottom/height
+                if class_id in gv.DETECTIONS:
+                    color = transfer("")
+                    # Si la persona detectada tiene algo verde
+                    if color == "1":
+                        check = True
+                        lower = np.array([0, 0, 330], np.uint8)
+                        upper = np.array([180, 255, 30], np.uint8)
+                        break
+                    # Si la persona detectada tiene algo rojo
+                    elif color == "2":
+                        check = True
+                        lower = np.array([0, 0, 90], np.uint8)
+                        upper = np.array([180, 255, 150], np.uint8)
+                        break
+                    # Si la persona detectada tiene algo azul
+                    elif color == "3":
+                        check = True
+                        lower = np.array([0, 0, 210], np.uint8)
+                        upper = np.array([180, 255, 270], np.uint8)
+                        break
+                    else:
+                        check = False
 
-        if check:
-            cropped_img = img[y1:y2, x1:x2,:]
-            hsv = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
-            mask = cv2.inRange(hsv, lower, upper)
-            unique_counts = dict(zip(np.unique(mask, return_counts=True)))
-            if unique_counts['0'] >= 30:
-                center = detection_center(detection)
-                robot.set_motors(
-                    float(speed + turn_gain * center[0]),
-                    float(speed - turn_gain * center[0])
-                )
-            else:
-                robot.set_motors(0,0)
+            if check:
+                cropped_img = img[y1:y2, x1:x2,:]
+                hsv = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
+                mask = cv2.inRange(hsv, lower, upper)
+                unique_counts = dict(zip(np.unique(mask, return_counts=True)))
+                if unique_counts['0'] >= 30:
+                    center = detection_center(detection)
+                    robot.set_motors(
+                        float(speed + turn_gain * center[0]),
+                        float(speed - turn_gain * center[0])
+                    )
+                else:
+                    robot.set_motors(0,0)
 
 
                     
