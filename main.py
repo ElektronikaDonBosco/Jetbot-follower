61412f9b59b6df62203f748a9b96f887a47f670b
Mikel Aldalur <mikelalda13@gmail.com>
2023-07-05 22:36:29 +0200

    Display not suported

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def detection_center(detection):
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2 and 'csi://0' for csi
-display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
-render_img = False
+# display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
+# render_img = False
 robot = Robot()
 
 speed = 0.5
@@ -47,9 +47,9 @@ def main():
         img = camera.Capture()
         height, width, channels = img.shape
         detections = net.Detect(img)
-        if render_img:
-            display.Render(img)
-            display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+        # if render_img:
+        #     display.Render(img)
+        #     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
         for detection in detections:
             class_id = names[detection.ClassID]
             x1 = detection.Left/width 
